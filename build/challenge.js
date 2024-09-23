ce6456ee82675d04d92a6aa458057508abf7ed73
hannahazan <hannahazan2@gmail.com>
2024-09-23 17:14:59 +0200

    TS/Quest-2-Challenge

---
+++ build/challenge.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const prettyPrintWilder = (users) => {
+    users.map((user) => {
+        console.log(`${user.name} is ${user.age} years old`);
+    });
+};
+const wilders = [];
+const user1 = { name: "Pierre", age: 23 };
+const user2 = { name: "Paul", age: 25 };
+const user3 = { name: "Jacques", age: 25 };
+wilders.push(user1);
+wilders.push(user2);
+wilders.push(user3);
+prettyPrintWilder(wilders);
